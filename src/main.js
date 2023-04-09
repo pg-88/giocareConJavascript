9d353dd2bee09e2a8c40ebfe1c5769f55297fcc0
Pietro Grigolo <grigolopi@gmail.com>
2023-04-09 09:57:06 +0200

    fileReader

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ function getMain(){
     console.log(document.getElementsByTagName('main'));
     let m = document.getElementsByTagName('main')[0];
     console.log(m);
-    
+    console.log(window);
     if( m.hasChildNodes()){
-        let target = m.lastChild();
+        let target = m.lastElementChild;//uso last element per avere l'ultimo nodo HTML appeso al nodo.
+        //last child ritorna l'ultimo 'figlio' del nodo 
+        //che può non essere un oggetto html ma anche solo un nodo di testo
+
         console.log(target);
+        let test = document.createElement("p");
+        test.innerHTML = "ciao";
+        target.appendChild(test);
     }else console.log("nope... found nothing!");
 }
 
